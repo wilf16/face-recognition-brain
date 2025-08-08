a47a38d85cab151867cb7fb11c5efb0c62bae9e7
Wilfred Anorma <wilfred.anorma@zuhlke.com>
2025-08-08 17:47:46 +0800

    build components basically the layout withou functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 import './App.css'
+import Navigation from './components/Navigation/Navigation.jsx'
+import Logo from './components/Logo/Logo.jsx'
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.jsx'
+import Rank from './components/Rank/Rank.jsx'
+import FaceRecognition from './FaceRecognition.jsx'
 
 function App() {
   return (
-    <>
-      <h1>FaceRecognitionBrain</h1>
-    </>
+    <div>
+      <div id="particles-js" className='particles'></div>
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+      {
+        /* 
+        
+        <FaceRecognition />*/
+      }
+      
+    </div>
   )
 }
+
+particlesJS.load('particles-js', './src/assets/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
 
 export default App
---
+++ src/components/Rank/Rank.jsx
@@ -0,0 +1,14 @@
+const Rank = () => {
+    return (
+        <div>
+            <div className='center white f3'>
+                {'Wilf, your current rank is...'}
+            </div>
+            <div className='center white f1'>
+                {'#5'}
+            </div>
+        </div>
+    );
+}
+
+export default Rank;
