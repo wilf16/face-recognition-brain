7e0186b33cbf17fcd90d3cdb216df381cae24e65
Wilfred Anorma <wilfred.anorma@zuhlke.com>
2025-08-11 16:18:32 +0800

    implement clarifai face detect functionality although it is not working completely because of an error with Clarifai sdk not defining global

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import Navigation from './components/Navigation/Navigation.jsx'
 import Logo from './components/Logo/Logo.jsx'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.jsx'
 import Rank from './components/Rank/Rank.jsx'
-import FaceRecognition from './FaceRecognition.jsx'
 import SignIn from './components/SignIn/SignIn.jsx'
 import Register from './components/Register/Register.jsx'
-import { useState, useEffect } from 'react'
+import FaceRecognition from './components/FaceRecognition/FaceRecognition.jsx'
+import { useState, useEffect, useState } from 'react'
+// import Clarifai from 'clarifai'
 
 function App() {
   const [route, setRoute] = useState('signin');
   const [isSignedIn, setIsSignedIn] = useState(false);
+  const [input, setInput] = useState('');
+  const [imageUrl, setImageUrl] = useState(null);
+  const [box, setBox] = useState({});
 
   const onRouteChange = (newRoute) => {
     if (newRoute === 'signout') {
@@ -23,11 +27,52 @@ function App() {
     setRoute(newRoute);
   }
 
+
   useEffect(() => {
     particlesJS.load('particles-js', './src/assets/particles.json', function() {
       console.log('callback - particles.js config loaded');
     });
   }, []);
+
+  // const app = new Clarifai.App({
+  //   apiKey: '7a19cb31b2d448f48b6f9024e97b4ba4'
+  // });
+  function calculateFaceLocation(data) {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  function displayFaceBox(box) {
+    setBox(box);
+  }
+
+  const onInputChange = (event) => {
+    setInput(event.target.value);
+  }
+
+  const onButtonSubmit = () => {
+    setImageUrl(input);
+    displayFaceBox({
+      leftCol: 131.729,
+      topRow: 146.954,
+      rightCol: 141.376,
+      bottomRow: 123.161
+    });
+    // app.models.predict(Clarifai.FACE_DETECT_MODEL, input)
+    // .then(response => calculateFaceLocation(response))
+    // .then(box => displayFaceBox(box))
+    // .catch(err => console.log(err));
+  }
+
+
 
   return (
     <div>
@@ -38,8 +83,10 @@ function App() {
         ? <div>
             <Logo />
             <Rank />
-            <ImageLinkForm />
-            <FaceRecognition /> 
+            <ImageLinkForm 
+              onInputChange={onInputChange} 
+              onButtonSubmit={onButtonSubmit} />
+            <FaceRecognition box={box} imageUrl={imageUrl} />
           </div>
         : <div>
             { route === 'signin' 
