1d31e99405a9a1718f004afdde69ded6aa873616
Wilfred Anorma <wilfred.anorma@zuhlke.com>
2025-08-11 16:18:32 +0800

    implement clarifai face detect functionality although it is not working completely because of an error with Clarifai sdk not defining global

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,14 @@ import Navigation from './components/Navigation/Navigation.jsx'
 import Logo from './components/Logo/Logo.jsx'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.jsx'
 import Rank from './components/Rank/Rank.jsx'
-import { useEffect } from 'react'
+import FaceRecognition from './components/FaceRecognition/FaceRecognition.jsx'
+import { useEffect, useState } from 'react'
+// import Clarifai from 'clarifai'
 
 function App() {
+  const [input, setInput] = useState('');
+  const [imageUrl, setImageUrl] = useState(null);
+  const [box, setBox] = useState({});
 
   useEffect(() => {
     particlesJS.load('particles-js', './src/assets/particles.json', function() {
@@ -13,16 +18,56 @@ function App() {
     });
   }, []);
 
+  // const app = new Clarifai.App({
+  //   apiKey: '7a19cb31b2d448f48b6f9024e97b4ba4'
+  // });
+  function calculateFaceLocation(data) {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  function displayFaceBox(box) {
+    setBox(box);
+  }
+
+  const onInputChange = (event) => {
+    setInput(event.target.value);
+  }
+
+  const onButtonSubmit = () => {
+    setImageUrl(input);
+    displayFaceBox({
+      leftCol: 131.729,
+      topRow: 146.954,
+      rightCol: 141.376,
+      bottomRow: 123.161
+    });
+    // app.models.predict(Clarifai.FACE_DETECT_MODEL, input)
+    // .then(response => calculateFaceLocation(response))
+    // .then(box => displayFaceBox(box))
+    // .catch(err => console.log(err));
+  }
+
+
+
   return (
     <div>
       <div id="particles-js" className='particles'></div>
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
-      { /*<FaceRecognition />*/
-      }
-      
+      <ImageLinkForm 
+      onInputChange={onInputChange} 
+      onButtonSubmit={onButtonSubmit} />
+      <FaceRecognition box={box} imageUrl={imageUrl} />
     </div>
   )
 }
--- src/components/ImageLinkForm/ImageLinkForm.jsx
+++ src/components/ImageLinkForm/ImageLinkForm.jsx
@@ -1,5 +1,6 @@
 import './ImageLinkForm.css'
-const ImageLinkForm = () => {
+
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div>
       <p className='center f3'>
@@ -7,8 +8,10 @@ const ImageLinkForm = () => {
       </p>
       <div className='center'>
         <div className='form center pa4 br3 shadow-5'>
-          <input className='f4 pa2 w-70 center' type='text' />
-          <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
+          <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange} />
+          <button 
+          className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple' 
+          onClick={onButtonSubmit}>Detect</button>
         </div>
       </div>
     </div>
