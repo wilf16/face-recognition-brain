5a0d08391ec23213d6821d4298d5fc9d0c070815
Wilfred Anorma <wilfred.anorma@zuhlke.com>
2025-08-14 16:19:26 +0800

    fix duplicated codes from merging

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,12 @@ function App() {
   const [imageUrl, setImageUrl] = useState(null);
   const [box, setBox] = useState({});
 
+  useEffect(() => {
+    particlesJS.load('particles-js', './src/assets/particles.json', function() {
+      console.log('callback - particles.js config loaded');
+    });
+  }, []);
+
   const onRouteChange = (newRoute) => {
     if (newRoute === 'signout') {
       setIsSignedIn(false);
@@ -27,33 +33,6 @@ function App() {
     setRoute(newRoute);
   }
 
-
-  useEffect(() => {
-    particlesJS.load('particles-js', './src/assets/particles.json', function() {
-      console.log('callback - particles.js config loaded');
-    });
-  }, []);
-
-  // const app = new Clarifai.App({
-  //   apiKey: '7a19cb31b2d448f48b6f9024e97b4ba4'
-  // });
-  function calculateFaceLocation(data) {
-    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementById('inputimage');
-    const width = Number(image.width);
-    const height = Number(image.height);
-    return {
-      leftCol: clarifaiFace.left_col * width,
-      topRow: clarifaiFace.top_row * height,
-      rightCol: width - (clarifaiFace.right_col * width),
-      bottomRow: height - (clarifaiFace.bottom_row * height)
-    }
-  }
-
-  function displayFaceBox(box) {
-    setBox(box);
-  }
-
   const onInputChange = (event) => {
     setInput(event.target.value);
   }
@@ -72,11 +51,6 @@ function App() {
     // .catch(err => console.log(err));
   }
 
-
-
-  // const app = new Clarifai.App({
-  //   apiKey: '7a19cb31b2d448f48b6f9024e97b4ba4'
-  // });
   function calculateFaceLocation(data) {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputimage');
@@ -93,26 +67,6 @@ function App() {
   function displayFaceBox(box) {
     setBox(box);
   }
-
-  const onInputChange = (event) => {
-    setInput(event.target.value);
-  }
-
-  const onButtonSubmit = () => {
-    setImageUrl(input);
-    displayFaceBox({
-      leftCol: 131.729,
-      topRow: 146.954,
-      rightCol: 141.376,
-      bottomRow: 123.161
-    });
-    // app.models.predict(Clarifai.FACE_DETECT_MODEL, input)
-    // .then(response => calculateFaceLocation(response))
-    // .then(box => displayFaceBox(box))
-    // .catch(err => console.log(err));
-  }
-
-
 
   return (
     <div>
